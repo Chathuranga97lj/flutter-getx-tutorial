00ca15eec861a6fd45281849ddb47e8f16941696
Chathuranga97lj <chathuit97@gmail.com>
2023-03-02 11:26:54 +0530

    getx snack bar

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Snack Bar")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                  child: const Text("Show SnackBar"),
+                  onPressed: () {
+                    Get.snackbar("SnackBar", "This will be Snackbar Message");
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
