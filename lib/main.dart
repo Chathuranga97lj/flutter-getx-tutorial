b44a42d158eb4cd25544505c8c2dbbc48eb47604
Chathuranga97lj <chathuit97@gmail.com>
2023-03-03 13:39:04 +0530

    add| :

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getxtest1/my_controller.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,20 +9,40 @@ void main() {
 class MyApp extends StatelessWidget {
   //const MyApp({super.key});
 
+  // if used not GetX<Type of controller> crate instance inside the class
+  MyController myController = Get.put(MyController());
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        appBar: AppBar(title: const Text("Snack Bar")),
+        appBar: AppBar(title: const Text("GetX Controller Type")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              GetX<MyController>(
+                //init: MyController(), // if instance crate inside the class no need to do this
+                builder: (controller) {
+                  return Text(
+                    // "The value is ${controller.count}",
+                    "The value is ${myController.count}",
+                    style: const TextStyle(fontSize: 25),
+                  );
+                },
+              ),
               ElevatedButton(
-                  child: const Text("Show SnackBar"), onPressed: () {})
+                  child: const Text("Increase Value"),
+                  onPressed: () {
+                    // when instance is only builder method
+                    //Get.find<MyController>().increment();
+
+                    // when instance is create in the class
+                    myController.increment();
+                  })
             ],
           ),
         ),
